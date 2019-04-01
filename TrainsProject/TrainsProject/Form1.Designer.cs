d34c684df55ce994357481a3bcc8881d6d8a75b7
alexisvanderwilt <alexis@query.ai>
2019-04-01 13:39:19 -0500

    Created Design

--- TrainsProject/TrainsProject/Form1.Designer.cs
+++ TrainsProject/TrainsProject/Form1.Designer.cs
@@ -28,13 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.stationMapTable = new System.Windows.Forms.TableLayoutPanel();
+            this.buyStationButton = new System.Windows.Forms.Button();
+            this.buyTrainsButton = new System.Windows.Forms.Button();
+            this.CurrentMoneyTextBox = new System.Windows.Forms.TextBox();
+            this.ConsoleTextBox = new System.Windows.Forms.TextBox();
+            this.stationTable = new System.Windows.Forms.TableLayoutPanel();
+            this.trainTable = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // stationMapTable
+            // 
+            this.stationMapTable.ColumnCount = 10;
+            this.stationMapTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.Location = new System.Drawing.Point(12, 12);
+            this.stationMapTable.Name = "stationMapTable";
+            this.stationMapTable.RowCount = 10;
+            this.stationMapTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.stationMapTable.Size = new System.Drawing.Size(1347, 431);
+            this.stationMapTable.TabIndex = 0;
+            // 
+            // buyStationButton
+            // 
+            this.buyStationButton.ForeColor = System.Drawing.Color.Black;
+            this.buyStationButton.Location = new System.Drawing.Point(12, 449);
+            this.buyStationButton.Name = "buyStationButton";
+            this.buyStationButton.Size = new System.Drawing.Size(137, 51);
+            this.buyStationButton.TabIndex = 1;
+            this.buyStationButton.Text = "Buy Stations";
+            this.buyStationButton.UseVisualStyleBackColor = true;
+            // 
+            // buyTrainsButton
+            // 
+            this.buyTrainsButton.ForeColor = System.Drawing.Color.Black;
+            this.buyTrainsButton.Location = new System.Drawing.Point(12, 506);
+            this.buyTrainsButton.Name = "buyTrainsButton";
+            this.buyTrainsButton.Size = new System.Drawing.Size(137, 51);
+            this.buyTrainsButton.TabIndex = 2;
+            this.buyTrainsButton.Text = "Buy Trains";
+            this.buyTrainsButton.UseVisualStyleBackColor = true;
+            // 
+            // CurrentMoneyTextBox
+            // 
+            this.CurrentMoneyTextBox.BackColor = System.Drawing.Color.Purple;
+            this.CurrentMoneyTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentMoneyTextBox.ForeColor = System.Drawing.Color.White;
+            this.CurrentMoneyTextBox.Location = new System.Drawing.Point(155, 449);
+            this.CurrentMoneyTextBox.Name = "CurrentMoneyTextBox";
+            this.CurrentMoneyTextBox.Size = new System.Drawing.Size(262, 49);
+            this.CurrentMoneyTextBox.TabIndex = 3;
+            // 
+            // ConsoleTextBox
+            // 
+            this.ConsoleTextBox.Location = new System.Drawing.Point(435, 454);
+            this.ConsoleTextBox.Multiline = true;
+            this.ConsoleTextBox.Name = "ConsoleTextBox";
+            this.ConsoleTextBox.Size = new System.Drawing.Size(924, 162);
+            this.ConsoleTextBox.TabIndex = 4;
+            // 
+            // stationTable
+            // 
+            this.stationTable.ColumnCount = 2;
+            this.stationTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.stationTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.stationTable.Location = new System.Drawing.Point(12, 563);
+            this.stationTable.Name = "stationTable";
+            this.stationTable.RowCount = 2;
+            this.stationTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.stationTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.stationTable.Size = new System.Drawing.Size(405, 196);
+            this.stationTable.TabIndex = 5;
+            // 
+            // trainTable
+            // 
+            this.trainTable.ColumnCount = 5;
+            this.trainTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.trainTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.trainTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.trainTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.trainTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.trainTable.Location = new System.Drawing.Point(435, 622);
+            this.trainTable.Name = "trainTable";
+            this.trainTable.RowCount = 2;
+            this.trainTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.trainTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.trainTable.Size = new System.Drawing.Size(920, 137);
+            this.trainTable.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1367, 771);
+            this.Controls.Add(this.trainTable);
+            this.Controls.Add(this.stationTable);
+            this.Controls.Add(this.ConsoleTextBox);
+            this.Controls.Add(this.CurrentMoneyTextBox);
+            this.Controls.Add(this.buyTrainsButton);
+            this.Controls.Add(this.buyStationButton);
+            this.Controls.Add(this.stationMapTable);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel stationMapTable;
+        private System.Windows.Forms.Button buyStationButton;
+        private System.Windows.Forms.Button buyTrainsButton;
+        private System.Windows.Forms.TextBox CurrentMoneyTextBox;
+        private System.Windows.Forms.TextBox ConsoleTextBox;
+        private System.Windows.Forms.TableLayoutPanel stationTable;
+        private System.Windows.Forms.TableLayoutPanel trainTable;
     }
 }
 
--- TrainsProject/TrainsProject/Form1.cs
+++ TrainsProject/TrainsProject/Form1.cs
@@ -16,5 +16,10 @@ namespace TrainsProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
